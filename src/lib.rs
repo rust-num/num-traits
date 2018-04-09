830363024b28c67f4799a08d0b4997667f382936
Vincent Esche <regexident@gmail.com>
2018-04-09 10:21:51 +0200

    Added `MulAdd` and `MulAddAssign` traits

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,7 @@ pub use identities::{Zero, One, zero, one};
 pub use ops::inv::Inv;
 pub use ops::checked::{CheckedAdd, CheckedSub, CheckedMul, CheckedDiv, CheckedShl, CheckedShr};
 pub use ops::wrapping::{WrappingAdd, WrappingMul, WrappingSub};
+pub use ops::mul_add::{MulAdd, MulAddAssign};
 pub use ops::saturating::Saturating;
 pub use sign::{Signed, Unsigned, abs, abs_sub, signum};
 pub use cast::{AsPrimitive, FromPrimitive, ToPrimitive, NumCast, cast};
--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -2,3 +2,4 @@ pub mod saturating;
 pub mod checked;
 pub mod wrapping;
 pub mod inv;
+pub mod mul_add;
