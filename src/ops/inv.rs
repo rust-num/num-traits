eb04676f43499e02c0280cd7ad668033dfeb23dd
goldenMetteyya <esa281@nyu.edu>
2019-01-10 12:26:53 +0300

    Update inv.rs

--- src/ops/inv.rs
+++ src/ops/inv.rs
@@ -45,3 +45,15 @@ impl<'a> Inv for &'a f64 {
         1.0 / *self
     }
 }
+
+
+/// Generic trait to implement modular inverse
+pub trait ModInverse<R: Sized>: Sized {
+    /// Function to calculate the [modular multiplicative
+    /// inverse](https://en.wikipedia.org/wiki/Modular_multiplicative_inverse) of an integer *a* modulo *m*.
+    ///
+    /// TODO: references
+    /// Returns the modular inverse of `self`.
+    /// If none exists it returns `None`.
+    fn mod_inverse(self, m: R) -> Option<Self>;
+}
