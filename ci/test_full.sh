63047365be9494206e145c0bb59796dfeb54f104
Yoan Lecoq <yoanlecoq.io@gmail.com>
2019-09-11 12:26:10 +0200

    CI: make the condition for testing libm the same as u128/i128

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -8,21 +8,20 @@ echo Testing num-traits on rustc ${TRAVIS_RUST_VERSION}
 cargo build --verbose
 cargo test --verbose
 
-# test with std and libm
-cargo build --verbose --features "libm"
-cargo test --verbose --features "libm"
-
 # test `no_std`
 cargo build --verbose --no-default-features
 cargo test --verbose --no-default-features
 
-# test `no_std` with libm
-cargo build --verbose --no-default-features --features "libm"
-cargo test --verbose --no-default-features --features "libm"
-
-
-# test `i128`
 if [[ "$TRAVIS_RUST_VERSION" =~ ^(nightly|beta|stable)$ ]]; then
+    # test `i128`
     cargo build --verbose --features=i128
     cargo test --verbose --features=i128
+
+    # test with std and libm (libm build fails on Rust 1.26 and earlier)
+    cargo build --verbose --features "libm"
+    cargo test --verbose --features "libm"
+
+    # test `no_std` with libm (libm build fails on Rust 1.26 and earlier)
+    cargo build --verbose --no-default-features --features "libm"
+    cargo test --verbose --no-default-features --features "libm"
 fi
