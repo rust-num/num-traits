531d965cefc7bfad9566d505b03573fb1a7283a2
Josh Stone <cuviper@gmail.com>
2024-02-07 16:09:04 -0800

    Note the MSRV for total_cmp

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let ac = autocfg::new();
 
-    ac.emit_expression_cfg("1f64.total_cmp(&2f64)", "has_total_cmp");
+    ac.emit_expression_cfg("1f64.total_cmp(&2f64)", "has_total_cmp"); // 1.62
 
     ac.emit_expression_cfg("1u32.to_ne_bytes()", "has_int_to_from_bytes");
     ac.emit_expression_cfg("3.14f64.to_ne_bytes()", "has_float_to_from_bytes");
