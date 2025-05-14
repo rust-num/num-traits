8cb9d01e4f44e617ef8a94d4c1543559364e4e5d
Bas van Dijk <bas@dfinity.org>
2025-05-14 10:33:07 +0000

    Make the build reproducible

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let ac = autocfg::new();
+    let temp_dir = tempfile::tempdir().expect("failed to create temp dir");
+    let ac = autocfg::AutoCfg::with_dir(temp_dir.path()).unwrap();
 
     ac.emit_expression_cfg("1f64.total_cmp(&2f64)", "has_total_cmp"); // 1.62
 
